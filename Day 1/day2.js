46c5e9957d12f2689ec99a824012fb52c89b889e
Nithila Palett A <76938563+nithp@users.noreply.github.com>
2021-07-12 23:35:11 +0530

    Create day2.js

---
+++ Day 1/day2.js
@@ -0,0 +1,15 @@
+//ASSIGNMENT DAY 2
+
+console.log("for vertical stars")
+for(i=0; i<5; i++)
+{
+  console.log("*")
+}
+
+
+console.log("for stars in same row")
+star=""; //empty string
+for(i=0;i<5;i=i+1){
+  star= star + " *";
+}
+console.log(star);
