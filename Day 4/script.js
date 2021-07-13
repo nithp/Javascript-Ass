6535f4c4782ed5171ca69e20c245261cfd8bca1b
Nithila Palett A <76938563+nithp@users.noreply.github.com>
2021-07-13 18:32:07 +0530

    Create script.js

---
+++ Day 4/script.js
@@ -0,0 +1,8 @@
+
+function getdata() {
+    var name = document.getElementById("name")
+    var data = name.value;
+    var dart = document.getElementById("head")
+    dart.innerHTML ="Hey Welcome " + data + "! Have a great day";
+
+}
